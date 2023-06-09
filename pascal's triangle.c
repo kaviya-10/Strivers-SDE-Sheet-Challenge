b016d97969b8b75de62cf732a3a1b0ac8d8f8b31
kaviya-10 <113359561+kaviya-10@users.noreply.github.com>
2023-06-09 20:14:54 +0530

    Add files via upload

---
+++ pascal's triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void Pascal(int n)
+{
+    for (int i = 1; i <= n; i++,printf("\n"))
+    {
+
+        int p = 1;
+        for (int j = 1; j<= i; j++)
+        {
+            printf("%d ", p);
+            p = p * (i- j) / j;
+        }
+
+    }
+}
+
+int main()
+{
+    int n=5;
+    //scanf("%d",&n);
+    Pascal(n);
+    return 0;
+}
